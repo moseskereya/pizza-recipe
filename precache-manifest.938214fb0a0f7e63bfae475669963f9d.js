521fb61b47a1a8fdda3f0d6c71292222109484fb
MOSES <MOSESLAZARO2019@outlook.com>
2019-10-30 23:22:12 +0300

    Updates

---
+++ precache-manifest.938214fb0a0f7e63bfae475669963f9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6d5f17f917a37e499f4ccfc424c96b",
+    "url": "/pizza-recipe/index.html"
+  },
+  {
+    "revision": "37a5c550fb0307eacd70",
+    "url": "/pizza-recipe/static/css/main.708fc5e8.chunk.css"
+  },
+  {
+    "revision": "2b3a81c3ee5ce009402b",
+    "url": "/pizza-recipe/static/js/2.b9117d20.chunk.js"
+  },
+  {
+    "revision": "37a5c550fb0307eacd70",
+    "url": "/pizza-recipe/static/js/main.6b2c3ef7.chunk.js"
+  },
+  {
+    "revision": "a9bbed0a6c581366b271",
+    "url": "/pizza-recipe/static/js/runtime-main.91ecea43.js"
+  }
+]);
